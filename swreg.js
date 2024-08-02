ba760c060b5bf84d98c81bbb35a125aa22a2af49
Xandr215 <xanderpbuckner1@hotmail.com>
2024-08-02 23:46:56 +0000

    Create swreg.js

---
+++ swreg.js
@@ -0,0 +1,12 @@
+// main.js or a script in your HTML
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
