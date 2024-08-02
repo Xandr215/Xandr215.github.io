eaece58fdb9a26ecc697439304083563c904dff5
Xandr215 <xanderpbuckner1@hotmail.com>
2024-08-02 23:40:56 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+// service-worker.js
+const CACHE_NAME = 'site-cache';
+const urlsToCache = [
+  '/'
+  '/images/favicon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
